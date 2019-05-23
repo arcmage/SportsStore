4ff46089a4e0ef66d3109051c9cdaaaefaaa44dd
Mohammed Alquwaisem <arcmage666@live.com>
2019-05-23 04:37:21 +0300

    Set up DB connection

---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _context;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public IQueryable<Product> Products => _context.Products;
+    }
+}
