f521c4318b49af44e7453b1dfd69e82a6538cd4e
Mohammed Alquwaisem <arcmage666@live.com>
2019-06-17 13:08:18 +0300

    Added Admin functionalities to list and edit products

--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -7,13 +7,34 @@ namespace SportsStore.Models
 {
     public class EFProductRepository : IProductRepository
     {
-        private ApplicationDbContext _context;
+        private ApplicationDbContext context;
 
         public EFProductRepository(ApplicationDbContext ctx)
         {
-            _context = ctx;
+            context = ctx;
         }
 
-        public IQueryable<Product> Products => _context.Products;
+        public IQueryable<Product> Products => context.Products;
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.Products
+                    .FirstOrDefault(p => p.ProductID == product.ProductID);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+        }
     }
 }
